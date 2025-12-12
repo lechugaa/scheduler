ed5c7596d07637edbc4cd65797150e35c4506c98
Antonio Lechuga <antoniolechugarivera@gmail.com>
2025-12-12 11:39:23 -0600

    Add tailwind configuration

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = "static"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
